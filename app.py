c0050517a5305ac34a70f4b8b1cf9e2ce93bc214
Angelo Steffenel <lsteffenel@gmail.com>
2021-02-09 19:57:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://media2.giphy.com/media/eN2chYe9jwGw8/giphy.gif?cid=ecf05e47xneueabtv478wiipupx6g36obffm6ulg47vmw86b&rid=giphy.gif",
-    "https://media0.giphy.com/media/Z8BXzZrix8pMI/giphy.gif?cid=ecf05e47f55e7895c0c0e79c9f27271977d321dc3bcc10b0&rid=giphy.gif",
+    "https://i.giphy.com/media/eN2chYe9jwGw8/giphy.webp",
+    "https://i.giphy.com/media/Z8BXzZrix8pMI/giphy.webp",
     "https://i.giphy.com/media/hXCGdsSC3MKuqZv59G/giphy.webp",
     "https://i.giphy.com/media/dxqOkrl29R8ac/giphy.webp",
     "https://i.giphy.com/media/5ESbl4pWvj2M0/giphy.webp"
